ae88e237fefada7ced3564eb3f6a2d4f66d08236
Yogesh <yogesh@healerite.com>
2021-06-03 05:25:31 -0400

    add rest framework and quick start in settings.py install apps

--- tutorial02/settings.py
+++ tutorial02/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',# we added rest framework
+    'quickstart.apps.QuickstartConfig'#we added quickstart app QuickstartConfig it class of quickstart app files
 ]
 
 MIDDLEWARE = [
